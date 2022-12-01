e0a05db1afc36ccab93d5db19174c53a68830575
Igor Bulakh <igorbulakh1987@yandex.ru>
2022-12-01 22:20:41 +0300

    Task done

--- 1/quadratic_equation.cpp
+++ 1/quadratic_equation.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <cmath>
 
 int main()
 {
-    std::cout << "" << std::endl;
+//     Ax² + Bx + C = 0.
+    double a, b, c;
+    std::cin >> a >> b >> c;
+    double D = b * b - (4 * a * c);
+
+    if (a != 0)
+    {
+        if (D < 0)
+        {
+            std::cout << "" << std::endl;
+        }
+        else if (D == 0)
+        {
+            double x = -b / (2 * a);
+            std::cout << x << std::endl;
+        }
+        else if (D > 0)
+        {
+            double x1 = (-b + sqrt(D)) / (2 * a);
+            double x2 = (-b - sqrt(D)) / (2 * a);
+            std::cout << x1 << " " << x2 << std::endl;
+        }
+    }
+    else if (a == 0)
+    {
+        if (b != 0)
+        {
+            double x = -c / b;
+            std::cout << x << std::endl;
+        }
+        else
+        {
+            std::cout << "" << std::endl;
+        }
+    }
     return 0;
 }
