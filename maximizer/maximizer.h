3f33396f624c3a78cb16663a2a7984f03dfc9feb
Igor Bulakh <igorbulakh1987@yandex.ru>
2023-04-12 13:37:18 +0300

    Update .gitignore and maximizer task is solved

---
+++ maximizer/maximizer.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void UpdateIfGreater(const int& a, int& b)
+{
+    if (a > b)
+    {
+        b = a;
+    }
+}
---
+++ maximizer/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "maximizer.h"
+
+using namespace std;
+
+int main()
+{
+    int a = 4;
+    int b = 2;
+    UpdateIfGreater(a, b);
+    cout << a << endl;
+    cout << b << endl;
+    return 0;
+}
