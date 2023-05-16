a8106d99cfdcbe8f89ed149b5908ef683b65011e
Igor Bulakh <igorbulakh1987@yandex.ru>
2023-05-16 16:19:27 +0300

    Lecture title task is solved

---
+++ lecture_title/lecture_title.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Specialization
+{
+    string value;
+
+    explicit Specialization(const string& new_value)
+    {
+        value = new_value;
+    }
+};
+
+struct Course
+{
+    string value;
+
+    explicit Course(const string& new_value)
+    {
+        value = new_value;
+    }
+};
+
+struct Week
+{
+    string value;
+
+    explicit Week(const string& new_value)
+    {
+        value = new_value;
+    }
+};
+
+struct LectureTitle {
+  string specialization;
+  string course;
+  string week;
+
+  LectureTitle(Specialization new_spec, Course new_course, Week new_week)
+  {
+    specialization = new_spec.value;
+    course = new_course.value;
+    week = new_week.value;
+  }
+};
+
+// int main()
+// {
+//     LectureTitle title(
+//         Specialization("C++"),
+//         Course("White belt"),
+//         Week("4th")
+//     );
+//     return 0;
+// }
