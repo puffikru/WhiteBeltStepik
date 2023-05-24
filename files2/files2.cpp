1756136a04305d28938309df0ac195097098653e
Igor Bulakh <igorbulakh1987@yandex.ru>
2023-05-24 13:57:19 +0300

    Working with files 2 task is solved

---
+++ files2/files2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+    string line;
+    if (input)
+    {
+        while (getline(input, line))
+        {
+            output << line << endl;
+        }
+    }
+    return 0;
+}
